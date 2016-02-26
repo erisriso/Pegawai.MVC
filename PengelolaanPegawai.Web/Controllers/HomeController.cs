719959312a011efd486199746e2a8c7e590ef57f
erisriso <erisriso@gmail.com>
2016-02-27 02:48:23 +0700

    Added files via upload

---
+++ PengelolaanPegawai.Web/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PengelolaanPegawai.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        
+        // GET: PageNotFound
+        public ActionResult PageNotFound()
+        {
+            return View();
+        }
+    }
+}
