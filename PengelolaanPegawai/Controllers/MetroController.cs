762f42933d7c69cf000af018e79fddb191017f74
ERIS RISO <erisriso@gmail.com>
2016-03-02 19:47:58 +0700

    Added files via upload

---
+++ PengelolaanPegawai/Controllers/MetroController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PengelolaanPegawai.Web.Controllers
+{
+    public class MetroController : Controller
+    {
+        // GET: Metro
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult UI() { return View(); }
+        public ActionResult Forms() { return View(); }
+        public ActionResult Charts() { return View(); }
+        public ActionResult Tables() { return View(); }
+
+        public ActionResult Login() { return View(); }
+
+        public ActionResult Latihan()
+        {
+            return View();
+        }
+    }
+}
---
+++ PengelolaanPegawai/Models/LoginViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PengelolaanPegawai.Web.Models
+{
+    public class LoginViewModel
+    {
+        public string Username { set; get; }
+        public string Password { set; get; }
+        public bool IsRemember { set; get; }
+    }
+}
